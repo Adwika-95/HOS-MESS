2a1f6116374b239f00ef612d193690a905e79a7a
Adwika <sahrawatadwika@gmail.com>
2025-11-03 13:12:37 +0530

    🔐 Added Firebase configuration for Caterer OTP login

---
+++ frontend/src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyAWHaoY4fwunTIx7u6-QxAwI4eFUCAbit0",
+  authDomain: "hos-mess.firebaseapp.com",
+  projectId: "hos-mess",
+  storageBucket: "hos-mess.firebasestorage.app",
+  messagingSenderId: "106816714519",
+  appId: "1:106816714519:web:6dbe59a3d34df6c1b1a952",
+  measurementId: "G-50GQBL5R2T"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
